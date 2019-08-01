9d2b693cfde4184b72abd632017a8946d5ae0f61
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-08-01 22:57:20 +0600

    verifyemailUrl added to site table

--- core/site.table.js
+++ core/site.table.js
@@ -4,5 +4,6 @@ var Schema = mongoose.Schema;
 var siteSchema = new Schema({
     sitename: { type: String, default: '' },
     tenantstring: { type: String, default: '' },
+    verifyEmailUrl: { type: String, default: '' }
 });
 module.exports = mongoose.model('site', siteSchema);
